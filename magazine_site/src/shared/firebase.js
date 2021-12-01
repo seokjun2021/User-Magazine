302bf8840d6a1317401a2c7793253f7dc034f775
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-12-02 04:16:18 +0900

    파이어베이스 연결

---
+++ magazine_site/src/shared/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+ const firebaseConfig = {
+
+        apiKey: "AIzaSyCHrUtcJE01MNDrIP-czmhhmtLWtxH5dIo",
+        authDomain: "my-magazine-f9c94.firebaseapp.com",
+        projectId: "my-magazine-f9c94",
+        storageBucket: "my-magazine-f9c94.appspot.com",
+        messagingSenderId: "906575634137",
+        appId: "1:906575634137:web:b7886c07c67ed128b2e8c8",
+        measurementId: "G-61TEQRN0FW",
+
+}
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+export { auth };
+
+
+
+
+
+
+
+
